1d955bd7e9981d0fc71f594eb2f5114c8bc91933
dudemaiaf <dudu_maia13@hotmail.com>
2017-06-01 00:40:09 -0400

    Add files via upload

---
+++ ComportamentoMagico.java
@@ -0,0 +1,11 @@
+package comportamentos;
+
+public interface ComportamentoMagico {
+	//invisibilidade
+	//ultrarapidez
+	public void setInvisibilidade (boolean invisibilidade);
+	public boolean getInvisibilidade ();
+	
+	public void setUltrarapidez (boolean ultrarapidez);
+	public boolean getUltrarapidez ();
+}
