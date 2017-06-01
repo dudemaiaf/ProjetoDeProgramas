b01d6567dedaafadf867caa93f583da2a4819adf
dudemaiaf <dudu_maia13@hotmail.com>
2017-06-01 00:40:23 -0400

    Add files via upload

---
+++ ComportamentoHeroico.java
@@ -0,0 +1,15 @@
+package comportamentos;
+
+public interface ComportamentoHeroico {
+	//atacar
+	//defender
+	//saltar
+	public void setAtacar (boolean atacar);
+	public boolean getAtacar ();
+	
+	public void setDefender (boolean defender);
+	public boolean getDefender ();
+	
+	public void setSaltar (boolean saltar);
+	public boolean getSaltar ();
+}
