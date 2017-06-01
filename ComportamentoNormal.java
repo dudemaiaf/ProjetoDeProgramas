bac7698c6e8f6b4b290d79312989d923cc535183
dudemaiaf <dudu_maia13@hotmail.com>
2017-06-01 00:39:38 -0400

    Add files via upload

---
+++ ComportamentoNormal.java
@@ -0,0 +1,12 @@
+package comportamentos;
+
+public interface ComportamentoNormal {
+	public void setAndar (boolean andar);
+	public boolean getAndar ();
+
+	public void setUsarItem (boolean usarItem);
+	public boolean getUsarItem ();
+
+	public void setGuardarItem (boolean guardarItem);
+	public boolean getGuardarItem ();
+}
